4a12e73f1c61068e45283d28791f3050b02851b9
Steve Heim <heim.steve@gmail.com>
2019-04-17 10:37:55 +0200

    package all extras into pMap

--- computeQ2Dtest.py
+++ computeQ2Dtest.py
@@ -7,12 +7,13 @@ x0 = [0,1.5,0,0]
 p['total_energy'] = computeTotalEnergy(x0,p)
 pMap.p = p
 pMap.x = x0
+pMap.sa2xp = mapSA2xp_height_angle
 
 s_table = np.linspace(0,1,4)
 a_table = np.linspace(0,np.pi/4,3)
 
-from viability import computeQ2D
-QMap = computeQ2D(s_table, a_table, pMap, mapSA2xp_height_angle)
+from viability import computeQ_2D
+QMap, Q_F = computeQ_2D(s_table, a_table, pMap)
 # plt.plot(sol.t,sol.y[0])
 # plt.plot(sol.t,sol.y[1], color='green')
 # # plt.plot(sol.t,sol.y[0])
--- viability.py
+++ viability.py
@@ -5,7 +5,7 @@ import numpy as np
 # from utilities import map2e, map2x
 from slip import map2x, map2e
 
-def computeQ2D(s_table, a_table, pMap,mapSA2xp):
+def computeQ_2D(s_table, a_table, pMap):
     ''' Compute the transition map of a system with 1D state and 1D action
 
     NOTES
@@ -19,17 +19,32 @@ def computeQ2D(s_table, a_table, pMap,mapSA2xp):
     # compute each combination, store result in a huge matrix
     
     # initialize 1D, reshape later
-    QMap = np.zeros((s_table.size*a_table.size,1))
+    Q_map = np.zeros((s_table.size*a_table.size,1))
+    Q_F = Q_map
     # TODO: also compute transitions diff maps etc.
-    # QTransition = QMap
+    # QTransition = Q_map
 
     for idx, state_action in enumerate(it.product(s_table,a_table)):
-        x,p = mapSA2xp(state_action,pMap.x,pMap.p)
+        x,p = pMap.sa2xp(state_action,pMap.x,pMap.p)
 
         x_next, failed = pMap(x,p)
 
         if not failed:
             s_next = map2e(x_next,p)
-            QMap[idx] = s_next
+            # note: Q_map is implicitly already excluding transitions that
+            # move straight to a failure. While this is not equivalent to the
+            # algorithm in the paper, for our systems it is a bit more efficient
+            Q_map[idx] = s_next
+        else:
+            Q_F[idx] = 1
+
     
-    return QMap.reshape((s_table.size,a_table.size))
+    return ( Q_map.reshape((s_table.size,a_table.size)), # only 2D
+    Q_F.reshape((s_table.size,a_table.size)) )
+
+def projectQ2S_2D(Q):
+    S = np.zeros((Q.shape[0],1))
+    for sdx, val in enumerate(S):
+        if sum(Q[sdx,:]) > 0:
+            S[sdx] = 1
+    return S
