81eb947b9c7dbe3036260e2f987fb68705f53331
Charlotte Le Mouel <lemouel@science.mpi-stuttgart.mpg.de>
2019-04-24 11:07:43 +0200

    explanation of the initial conditions

--- test.py
+++ test.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 
 p = {'mass':80.0, 'stiffness':8200.0, 'resting_length':1.0, 'gravity':9.81,
     'angle_of_attack':1/5*np.pi}
-x0 = np.array([0, 0.85, 5.5, 0, 0, 0])
+x0 = np.array([0, # x position
+    0.85, # y position
+    5.5, # x velocity
+    0, # y velocity
+    0, # foot position x
+    0]) # foot position y
 p['total_energy'] = compute_total_energy(x0, p)
 x0 = reset_leg(x0, p)
 sol = step(x0, p)
