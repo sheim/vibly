c875d5ffc45311f47925199e92a1f51c9414578b
Steve Heim <heim.steve@gmail.com>
2019-04-26 10:23:26 +0200

    catch error in test (did not reset leg before computing energy!)

--- demos/test.py
+++ demos/test.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 p = {'mass':80.0, 'stiffness':8200.0, 'resting_length':1.0, 'gravity':9.81,
     'angle_of_attack':1/5*np.pi}
 x0 = np.array([0, 0.85, 5.5, 0, 0, 0])
-p['total_energy'] = compute_total_energy(x0, p)
 x0 = reset_leg(x0, p)
+p['total_energy'] = compute_total_energy(x0, p)
 sol = step(x0, p)
 
 plt.plot(sol.y[0], sol.y[1], color='orange')
