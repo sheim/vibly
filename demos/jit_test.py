9717ce64de5299e3e3632b1a6762b1131d6737dd
Steve Heim <heim.steve@gmail.com>
2019-04-26 09:47:47 +0200

    clean up and package. You can now install locally with `pip install -e .`

--- demos/jit_test.py
+++ demos/jit_test.py
@@ -1,6 +1,7 @@
 from slip import *
 import numpy as np
 import matplotlib.pyplot as plt
+import timeit
 
 p = {'mass':80.0, 'stiffness':8200.0, 'resting_length':1.0, 'gravity':9.81,
     'angle_of_attack':1/5*np.pi}
@@ -9,5 +10,6 @@ p['total_energy'] = compute_total_energy(x0, p)
 x0 = reset_leg(x0, p)
 sol = step(x0, p)
 
-plt.plot(sol.y[0], sol.y[1], color='orange')
-plt.show()
+print(timeit.timeit('lambda x0, p: step', globals=globals()),number=10000)
+
+#################
---
+++ demos/test.py
@@ -0,0 +1,13 @@
+from slippy.slip import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+p = {'mass':80.0, 'stiffness':8200.0, 'resting_length':1.0, 'gravity':9.81,
+    'angle_of_attack':1/5*np.pi}
+x0 = np.array([0, 0.85, 5.5, 0, 0, 0])
+p['total_energy'] = compute_total_energy(x0, p)
+x0 = reset_leg(x0, p)
+sol = step(x0, p)
+
+plt.plot(sol.y[0], sol.y[1], color='orange')
+plt.show()
