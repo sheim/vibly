94719838c94b31d071e694b9b063ed2d9d6db337
Steve Heim <heim.steve@gmail.com>
2019-04-26 08:46:07 +0200

    make sure to initialize state as np array

--- computeQ2Dtest.py
+++ computeQ2Dtest.py
@@ -7,15 +7,15 @@ from slip import *
 
 p = {'mass':80.0, 'stiffness':8200.0, 'resting_length':1.0, 'gravity':9.81,
 'angle_of_attack':1/5*np.pi}
-x0 = [0, 0.85, 5.5, 0, 0, 0]
+x0 = np.array([0, 0.85, 5.5, 0, 0, 0])
 x0 = reset_leg(x0, p)
 p['total_energy'] = compute_total_energy(x0, p)
 poincare_map.p = p
 poincare_map.x = x0
 poincare_map.sa2xp = mapSA2xp_height_angle
 
-s_grid = np.linspace(0.4, 1, 4)
-a_grid = np.linspace(0, -np.pi/4, 5)
+s_grid = np.linspace(0.5, 0.6, 4)
+a_grid = np.linspace(20/180*np.pi, 35/180*np.pi, 5)
 
 from viability import compute_Q_2D, compute_QV_2D
 Q_map, Q_F = compute_Q_2D(s_grid, a_grid, poincare_map)
@@ -26,6 +26,8 @@ Q_V, S_V = compute_QV_2D(Q_map, grids)
 
 np.savez('test_computeQ2D.npz',s_grid, a_grid, Q_map, Q_F, Q_V, S_V)
 
+plt.imshow(Q_map)
+plt.show()
 # plt.plot(sol.t,sol.y[0])
 # plt.plot(sol.t,sol.y[1], color='green')
 # # plt.plot(sol.t,sol.y[0])
