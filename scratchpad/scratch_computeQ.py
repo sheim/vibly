f2670fa495ada4aa7fc18de1e4c76c3d2d42c7d4
Steve Heim <heim.steve@gmail.com>
2019-05-07 11:45:25 +0200

    add low_res dataset, and some scratch code

---
+++ scratchpad/scratch_computeQ.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from slippy.slip import *
+from slippy.viability import compute_Q_2D, compute_QV_2D
+
+p = {'mass':80.0, 'stiffness':8200.0, 'resting_length':1.0, 'gravity':9.81,
+'angle_of_attack':1/5*np.pi}
+x0 = np.array([0, 0.85, 5.5, 0, 0, 0])
+x0 = reset_leg(x0, p)
+p['total_energy'] = compute_total_energy(x0, p)
+poincare_map.p = p
+poincare_map.x = x0
+poincare_map.sa2xp = mapSA2xp_height_angle
+
+s_grid = np.linspace(0.1, 1, 50)
+a_grid = np.linspace(-10/180*np.pi, 90/180*np.pi, 51)
+
+Q_map, Q_F = compute_Q_2D(s_grid, a_grid, poincare_map)
+grids = {'states':s_grid, 'actions':a_grid}
+
+# save file
+data2save = {"s_grid": s_grid, "a_grid": a_grid, "Q_map": Q_map, "Q_F": Q_F}
+np.savez('test.npz', **data2save)
