aea25250d81ad481e024e090a74d48314ae9849b
Steve Heim <heim.steve@gmail.com>
2019-05-07 11:46:52 +0200

    fix computeQ

--- demos/computeQ.py
+++ demos/computeQ.py
@@ -1,37 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import timeit
+from slippy.slip import *
+from slippy.viability import compute_Q_2D, compute_QV_2D
 
-# Compute Q using the 2D version
+p = {'mass':80.0, 'stiffness':8200.0, 'resting_length':1.0, 'gravity':9.81,
+'angle_of_attack':1/5*np.pi}
+x0 = np.array([0, 0.85, 5.5, 0, 0, 0])
+x0 = reset_leg(x0, p)
+p['total_energy'] = compute_total_energy(x0, p)
+poincare_map.p = p
+poincare_map.x = x0
+poincare_map.sa2xp = mapSA2xp_height_angle
 
-from slippy.viability import compute_Q_map
+s_grid = np.linspace(0.1, 1, 50)
+a_grid = np.linspace(-10/180*np.pi, 90/180*np.pi, 51)
 
-# use the same grid etc.
-
-# differently from the 2D version, you must pass s_grid and a_grid as a
-# vector of np.arrays (in 2D you pass the np.arrays directly).
-Q_map, Q_F = compute_Q((s_grid,), (a_grid,), poincare_map)
-
+Q_map, Q_F = compute_Q_2D(s_grid, a_grid, poincare_map)
 grids = {'states':s_grid, 'actions':a_grid}
-Q_V, S_V = compute_QV_2D(Q_map, grids)
 
 # save file
-data2save = {"s_grid": s_grid, "a_grid": a_grid, "Q_map": Q_map, "Q_F": Q_F,
-    "Q_V": Q_V, "S_V": S_V}
-np.savez('test_computeQ2D.npz', **data2save)
-
-plt.imshow(Q_map)
-plt.show()
-
-# Do the same thing with the ND version
-s_grid = np.linspace(0.1, 1, 5)
-a_grid = np.linspace(-10/180*np.pi, 90/180*np.pi, 5)
-
-Q_map, Q_F = compute_Q_map((s_grid,), (a_grid,), poincare_map)
-grids = {'states':s_grid, 'actions':a_grid}
-Q_V, S_V = compute_QV_2D(Q_map, grids)
-
-# save file
-data2save = {"s_grid": s_grid, "a_grid": a_grid, "Q_map": Q_map, "Q_F": Q_F,
-    "Q_V": Q_V, "S_V": S_V}
-np.savez('test_computeQ.npz', **data2save)
+data2save = {"s_grid": s_grid, "a_grid": a_grid, "Q_map": Q_map, "Q_F": Q_F}
+np.savez('test.npz', **data2save)
