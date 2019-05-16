6ca08d5d8f21cef94ebdc6464b0fa60da8228d2d
Steve Heim <heim.steve@gmail.com>
2019-05-16 13:30:41 +0200

    Load from pickled data (simplifies a lot of things). Also, replaced some reshape(-1) with ravel(), which should be faster (https://stackoverflow.com/questions/9057379/correct-and-efficient-way-to-flatten-array-in-numpy-in-python)

--- scratchpad/compute_measure.py
+++ scratchpad/compute_measure.py
@@ -1,28 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import pickle
+
+import GPy
 
 from slippy.slip import *
 import slippy.viability as vibly
 
-import GPy
+################################################################################
+# Load and unpack data
+################################################################################
+infile = open('../data/slip_map.pickle', 'rb')
+data = pickle.load(infile)
+infile.close()
 
-data = np.load('slip_map.npz')
 Q_map = data['Q_map']
+
 Q_F = data['Q_F']
+x0 = data['x0']
+poincare_map = data['P_map']
+p = data['p']
+grids = data['grids']
 
-grids = {}
-temp = data['grids']
-grids['states'] = temp.item().get('states')
-grids['actions'] = temp.item().get('actions')
-
-p = {'mass':80.0, 'stiffness':8200.0, 'resting_length':1.0, 'gravity':9.81, 'angle_of_attack':1/5*np.pi}
-x0 = np.array([0, 0.85, 5.5, 0, 0, 0])
-x0 = reset_leg(x0, p)
-p['total_energy'] = compute_total_energy(x0, p)
-Q_feas = vibly.get_feasibility_mask(feasible, mapSA2xp_height_angle, grids=grids, x0=x0, p0=p)
-
+################################################################################
+# Compute measure from grid for warm-start
+################################################################################
 Q_V, S_V = vibly.compute_QV(Q_map, grids)
+
+Q_feas = vibly.get_feasibility_mask(feasible, mapSA2xp_height_angle,
+            grids=grids, x0=x0, p0=p)
 S_M = vibly.project_Q2S(Q_V, grids, np.sum)
 #S_M = S_M / grids['actions'][0].size
 Q_M = vibly.map_S2Q(Q_map, S_M, Q_V)
@@ -30,17 +37,6 @@ plt.plot(S_M)
 plt.show()
 plt.imshow(Q_M, origin='lower')
 plt.show()
-# save file
-timestr = time.strftime("%Y_%m_%H_%M_%S")
-print("Finished at " + timestr)
-print(timestr)
-# data2save = {"grids": grids, "Q_map": Q_map, "Q_F": Q_F, "p" : p,
-#             "P_map" : poincare_map}
-# np.savez('slip_constr_', **data2save)
-
-# plt.imshow(Q_V, origin = 'lower')
-# plt.show()
-
 
 y = Q_M.flatten().T
 y_train = y.reshape(-1,1)
@@ -48,18 +44,19 @@ y_scale = np.max(np.abs(y_train), axis=0)[0]
 y_train = y_train / y_scale
 
 # TODO This is ugly af
+# * actually seems like it's not so bad: https://stackoverflow.com/questions/1208118/using-numpy-to-build-an-array-of-all-combinations-of-two-arrays
 X_train_1, X_train_2 = np.meshgrid(grids['actions'], grids['states'])
 X = np.column_stack((X_train_1.flatten(), X_train_2.flatten()))
 
 np.random.seed(1)
 idx = np.random.choice(9100, size=3000, replace=False)
 
-idx_save = np.argwhere(Q_V.flatten()).reshape(-1)
-idx_notfeas = np.argwhere(~Q_feas.flatten()).reshape(-1)
-idx_unsave = np.argwhere( Q_feas.flatten() & ~Q_V.flatten()).reshape(-1)
+idx_safe = np.argwhere(Q_V.flatten()).ravel()
+idx_notfeas = np.argwhere(~Q_feas.flatten()).ravel()
+idx_unsafe = np.argwhere( Q_feas.flatten() & ~Q_V.flatten()).ravel()
 
-idx_1 = np.random.choice(idx_save, size=1000, replace=False)
-idx_2 = np.random.choice(idx_unsave, size=500, replace=False)
+idx_1 = np.random.choice(idx_safe, size=1000, replace=False)
+idx_2 = np.random.choice(idx_unsafe, size=500, replace=False)
 
 idx = np.concatenate((idx_1, idx_2))
 
@@ -69,26 +66,26 @@ X_train = X[idx, :]
 #X_train = X_train[0:100,:]
 #y_train = y_train[0:100]
 
-kernel_1 = GPy.kern.Matern32(input_dim=2, variance=1., lengthscale=.5, ARD=True, name='kern1')
+kernel_1 = GPy.kern.Matern32(input_dim=2, variance=1., lengthscale=.5,
+        ARD=True, name='kern1')
 kernel_1.variance.constrain_bounded(1e-3, 1e4)
 
-kernel_2 = GPy.kern.RBF(input_dim=2, variance=1, lengthscale=.5, ARD=True, name='kern2')
+kernel_2 = GPy.kern.RBF(input_dim=2, variance=1, lengthscale=.5,
+        ARD=True, name='kern2')
 kernel_2.variance.constrain_bounded(1e-3, 1e4)
-
 
 # kernel_3 = GPy.kern.ChangePointBasisFuncKernel(input_dim=1, active_dims=0, changepoint=(.3,.6), variance=1, ARD=True, name='kern3')
 # kernel_4 = GPy.kern.ChangePointBasisFuncKernel(input_dim=1, active_dims=1, changepoint=(.5,.9), variance=1, ARD=True, name='kern4')
 
-
 kernel = kernel_1 + kernel_2
-
 
 # warp_f = GPy.util.warping_functions.TanhFunction(n_terms=2)
 # gp_prior = GPy.models.WarpedGP(X_train, y_train, kernel=kernel, warping_function=warp_f)
 # gp_prior.likelihood.variance = 0.01
 
 
-gp_prior = GPy.models.GPRegression(X_train, y_train, kernel=kernel, noise_var=0.01)
+gp_prior = GPy.models.GPRegression(X_train, y_train,
+        kernel=kernel, noise_var=0.01)
 
 gp_prior.likelihood.variance.constrain_bounded(1e-7, 1e-3)
 
@@ -130,7 +127,8 @@ plt.plot(np.linspace(0, np.pi/2, 500), mu+np.sqrt(s2)*2)
 plt.plot(np.linspace(0, np.pi/2, 500), mu-np.sqrt(s2)*2)
 
 
-plt.plot(grids['actions'][0].reshape(-1,), Q_M[idx_state,:].reshape(-1,) / y_scale)
+plt.plot(grids['actions'][0].reshape(-1,),
+        Q_M[idx_state,:].reshape(-1,) / y_scale)
 
 plt.show()
 
@@ -144,7 +142,8 @@ mu, s2 = gp_prior.predict(X_test)
 plt.plot(np.linspace(0, 1, 500), mu)
 plt.plot(np.linspace(0, 1, 500), mu+np.sqrt(s2)*2)
 plt.plot(np.linspace(0, 1, 500), mu-np.sqrt(s2)*2)
-plt.plot(grids['states'][0].reshape(-1,), Q_M[:,idx_action].reshape(-1,) / y_scale)
+plt.plot(grids['states'][0].reshape(-1,),
+        Q_M[:,idx_action].reshape(-1,) / y_scale)
 
 plt.show()
 
