b83e88bb6120cdea6e8fb62658931b87844c24f3
Steve Heim <heim.steve@gmail.com>
2019-06-27 15:52:05 +0200

    minor comments and cosmetics, add option to march safety_threshold (for exploring within safety-set)

--- scratchpad/active_sampling.py
+++ scratchpad/active_sampling.py
@@ -20,9 +20,7 @@ x0 = data['x0']
 p_proxy = data['p']
 grids = data['grids']
 
-################################################################################
-# Compute measure from grid for warm-start
-################################################################################
+# ** Compute measure from grid for warm-start
 Q_V_proxy, S_V_proxy = vibly.compute_QV(Q_map_proxy, grids)
 
 Q_feas = vibly.get_feasibility_mask(proxy_model.feasible, proxy_model.mapSA2xp_height_angle,
@@ -30,18 +28,16 @@ Q_feas = vibly.get_feasibility_mask(proxy_model.feasible, proxy_model.mapSA2xp_h
 S_M_proxy = vibly.project_Q2S(Q_V_proxy, grids, np.mean)
 Q_M_proxy = vibly.map_S2Q(Q_map_proxy, S_M_proxy, Q_V_proxy)
 
-seed_n = np.random.randint(1,100)
+seed_n = np.random.randint(1, 100)
 # seed_n = 18
 print("Seed: " + str(seed_n))
 np.random.seed(seed_n)
 estimator = estimation.MeasureEstimation(state_dim=1, action_dim=1, seed=seed_n)
 
 AS_grid = np.meshgrid(grids['actions'][0], grids['states'][0])
-# AS_grid = np.array([35.0/180*np.pi, 0.5]) #np.meshgrid(grids['actions'][0], grids['states'][0])
-# Q_M_proxy = 1
 estimator.prepare_data(AS_grid=AS_grid, Q_M=Q_M_proxy, Q_V=Q_V_proxy)
 
-initial_measure = .24
+initial_measure = .3
 estimator.prior_data['AS'] = np.atleast_2d(np.array([38/(180)*np.pi, .45]))
 estimator.prior_data['Q'] = np.array([[initial_measure]])
 
@@ -49,11 +45,8 @@ estimator.create_prior(load='./model/prior.npy', save=False)
 
 idx_safest = np.unravel_index(np.argmax(Q_M_proxy), Q_M_proxy.shape)
 
+# ** Marching Thresholds
 
-
-################################################################################
-# Exploration decay
-################################################################################
 
 def interpo(a, b, n):
     # assert n > 0 and n <=1
@@ -67,26 +60,22 @@ exploration_confidence_e = 0.85
 measure_confidence_s = 0.75
 measure_confidence_e = 0.85
 
-################################################################################
-# Stuff for comparing at the end
-################################################################################
+safety_threshold_s = 0.0
+safety_threshold_e = 0.0
+
+# ** Stuff for comparing at the end
 
 X_grid_1, X_grid_2 = np.meshgrid(grids['actions'], grids['states'])
 X_grid = np.column_stack((X_grid_1.flatten(), X_grid_2.flatten()))
 
-
 estimator.set_data_empty()
-
 estimator.set_grid_shape(X_grid, Q_map_proxy.shape)
 
 Q_V_prior = estimator.safe_level_set(safety_threshold = 0, confidence_threshold = measure_confidence)
 Q_M_prior, Q_M_s2_prior = estimator.Q_M()
 S_M_prior = estimator.project_Q2S(Q_V_prior)
 
-
-################################################################################
-# load ground truth
-################################################################################
+# ** load ground truth
 import slippy.slip as true_model
 # import slippy.nslip as true_model
 
@@ -105,27 +94,23 @@ grids = data['grids']
 # x0 is just a placeholder needed for the simulation
 true_model.mapSA2xp = lambda q, p: true_model.mapSA2xp_height_angle(q, x0, p)
 
-################################################################################
-# Compute measure from grid for warm-start
-################################################################################
+# ** Compute measure from grid for warm-start
 Q_V_true, S_V_true = vibly.compute_QV(Q_map_true, grids)
 Q_feas = vibly.get_feasibility_mask(true_model.feasible, true_model.mapSA2xp_height_angle,
             grids=grids, x0=x0, p0=p_true)
 S_M_true = vibly.project_Q2S(Q_V_true, grids, np.mean)
 Q_M_true = vibly.map_S2Q(Q_map_true, S_M_true, Q_V_true)
 
-################################################################################
-# Active Sampling part
-################################################################################
+# ** Active Sampling part
 
 s_grid_shape = list(map(np.size, grids['states']))
 s_bin_shape = tuple(dim+1 for dim in s_grid_shape)
 a_grid_shape = list(map(np.size, grids['actions']))
 a_bin_shape = tuple(dim+1 for dim in a_grid_shape)
-#### from GP approximation, choose parts of Q to sample
-
+# ** from GP approximation, choose parts of Q to sample
 verbose = 2
 np.set_printoptions(precision=4)
+
 
 def learn(estimator, s0, p_true, n_samples=100, X=None, y=None):
 
@@ -141,15 +126,14 @@ def learn(estimator, s0, p_true, n_samples=100, X=None, y=None):
 
     for ndx in range(n_samples):
 
-        if verbose:
-            print('iteration '+str(ndx+1))
-
-        Q_V = estimator.safe_level_set(safety_threshold=0, confidence_threshold=measure_confidence)
+        Q_V = estimator.safe_level_set(safety_threshold=0,
+                                       confidence_threshold=measure_confidence)
         S_M_0 = estimator.project_Q2S(Q_V)
 
         Q_M, Q_M_s2 = estimator.Q_M()
 
-        Q_V_explore = estimator.safe_level_set(safety_threshold=0, confidence_threshold=exploration_confidence)
+        Q_V_explore = estimator.safe_level_set(safety_threshold=0,
+                                       confidence_threshold=exploration_confidence)
 
         # TODO Alex: dont grid states, dont evalutate the whole sets
         # slice actions available for those states
@@ -159,36 +143,37 @@ def learn(estimator, s0, p_true, n_samples=100, X=None, y=None):
 
         thresh_idx = np.array(A_slice, dtype=bool)
 
-        if not thresh_idx.any(): # empty, pick the safest
+        if not thresh_idx.any():  # empty, pick the safest
             if verbose > 1:
-                print('taking safest')
+                print('taking safest on iteration '+str(ndx+1))
 
-            Q_V_prop = estimator.safe_level_set(safety_threshold=0, confidence_threshold=None)
+            Q_V_prop = estimator.safe_level_set(safety_threshold=0,
+                                           confidence_threshold=None)
             Q_prop_slice = np.copy(Q_V_prop[s0_idx, slice(None)])
 
-            a_idx = np.argmax(Q_prop_slice + np.random.randn(A_slice.shape[0]) * 0.01)
-        else: # not empty, pick one of these
-            if verbose > 1:
-                print('explore!')
+            a_idx = np.argmax(Q_prop_slice
+                              + np.random.randn(A_slice.shape[0])*0.01)
+        else:  # not empty, pick one of these
+            # if verbose > 1:
+            #     print('explore!')
             A_slice[~thresh_idx] = np.nan
             A_slice_s2[~thresh_idx] = np.nan
 
             exploration_heuristic = np.sqrt(A_slice_s2)
             a_idx = np.nanargmax(exploration_heuristic)
 
-
-        a = grids['actions'][0][a_idx] #+ (np.random.rand()-0.5)*np.pi/36
+        a = grids['actions'][0][a_idx]  #+ (np.random.rand()-0.5)*np.pi/36
         # apply action, get to the next state
         x0, p_true = true_model.mapSA2xp((s0, a), p_true)
         x_next, failed = true_model.poincare_map(x0, p_true)
         if failed:
             failed_samples[ndx] = True
             if verbose:
-                print("FAILED!")
+                print('FAILED on iteration '+str(ndx+1))
 
             S_M_safe = estimator.project_Q2S(Q_V_explore)
 
-            #TODO make dimensions work
+            # TODO make dimensions work
             s_next_idx = np.random.choice(np.where(S_M_safe > 0)[0])
             s_next = grids['states'][0][s_next_idx]
 
@@ -240,15 +225,19 @@ for ndx in range(steps):  # in case you want to do small increments
                                      exploration_confidence_e, ndx/steps)
     measure_confidence = interpo(measure_confidence_s,
                                  measure_confidence_e, ndx/steps)
+    safety_threshold = interpo(safety_threshold_s, safety_threshold_e,
+                               ndx/steps)
 
     estimator, s0 = learn(estimator, s0, p_true, n_samples=10, X=X, y=Y)
 
-    Q_V = estimator.safe_level_set(safety_threshold=0,
+    Q_V = estimator.safe_level_set(safety_threshold=safety_threshold,
                                    confidence_threshold=measure_confidence)
     Q_M, Q_M_s2 = estimator.Q_M()
     S_M_0 = estimator.project_Q2S(Q_V)
 
-    Q_V_exp = estimator.safe_level_set(safety_threshold=0, confidence_threshold=exploration_confidence)
+    Q_V_exp = estimator.safe_level_set(safety_threshold=0,
+                                       confidence_threshold=
+                                       exploration_confidence)
 
     fig = cplot.plot_Q_S(Q_V+Q_V_exp+Q_V_true*3, (S_M_0, S_M_true), grids,
                          samples=(estimator.gp.X, estimator.gp.Y),
