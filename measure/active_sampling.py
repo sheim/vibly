9cdb06bc2163c5bc1bf30a0c54d86902c466b3f7
Alexander von Rohr <vonrohr@is.mpg.de>
2019-09-24 17:50:00 +0200

    Parameter tuning on 4d hovership

--- measure/active_sampling.py
+++ measure/active_sampling.py
@@ -190,19 +190,11 @@ class MeasureLearner:
                     safe_idx = np.where(S_M_safe > 0)
                     s_next_idx = [np.random.choice(safe_idx[i]) for i in range(0, len(safe_idx))]
                     s_next = [self.grids['states'][i][s_next_idx[i]] for i in range(0,len(s_next_idx))]
-                    s_next_idx = np.array(s_next_idx)
                     s_next = np.array(s_next)
 
                 else:
                     # if the measure is 0 everywhere, we cannot recover anyway.
                     raise Exception('The whole measure is 0 now. There exits no action that is safe')
-                    # S_M_safe = estimation.project_Q2S(Q_V)
-                    #
-                    # s_next_idx = np.argmax(S_M_safe)
-                    # s_next_idx = np.unravel_index(s_next_idx, S_M_safe.shape)
-                    # s_next = [self.grids['states'][i][s_next_idx[i]] for i in range(0,len(s_next_idx))]
-                    # s_next_idx = np.array(s_next_idx)
-                    # s_next = np.array(s_next)
 
             measure = self.current_estimation.failure_value
         else:
