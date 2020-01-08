9c9dd1db39524523a54a9ca5fe1ca4fe45194db6
Steve Heim <heim.steve@gmail.com>
2020-01-08 11:08:40 +0100

    implement plotting of Poinc Section of measure in state-space, with overlay of states

--- plotting/guinea_plotters.py
+++ plotting/guinea_plotters.py
@@ -30,18 +30,7 @@ def interp_measure(s_bin, S_M, grids):
         return 0.0
 
 
-def ground_perturbations(ax, trajectories, S_M, grids, p, v_threshold=0.1,
-                         col_offset=0.65):
-    '''
-    Plot a series of trajectories, centered around level-ground as nominal
-    inputs:
-    trajectories: list of traj objects (sol to scipy.integrate.solve_ivp)
-    v_threshold: minimum safety, otherwise don't plot it
-    '''
-
-    # TODO redo this with trajectories colored by measure
-
-        # * get index of nominal trajectory, max step up/down
+def get_perturbation_indices(trajectories):
     index0 = 0
     max_up = 0
     max_down = 0
@@ -58,6 +47,22 @@ def ground_perturbations(ax, trajectories, S_M, grids, p, v_threshold=0.1,
             print("WARNING: no nominal trajectory!")
         num_up = index0-1
         num_down = len(trajectories)-index0
+    
+    return index0, max_up, max_down
+
+def plot_ground_perturbations(ax, trajectories, S_M, grids, p, v_threshold=0.1,
+                         col_offset=0.65):
+    '''
+    Plot a series of trajectories, centered around level-ground as nominal
+    inputs:
+    trajectories: list of traj objects (sol to scipy.integrate.solve_ivp)
+    v_threshold: minimum safety, otherwise don't plot it
+    '''
+
+    # TODO redo this with trajectories colored by measure
+
+    # * get index of nominal trajectory, max step up/down
+    index0, max_up, max_down = get_perturbation_indices(trajectories)
 
     # * plot step-ups
     mycmap = plt.get_cmap("Blues")
@@ -143,4 +148,105 @@ def waterfall_plot(fig, ax, X, Y, Z,
     fig.colorbar(lc)  # add colorbar, as the normalization is the same for all, it doesent matter which of the lc objects we use
 
 
+def get_max_measure(SM_list):
+    v_max = 0
+    for SM in SM_list:
+        if np.max(SM) > v_max:
+            v_max = np.max(SM)
+    return v_max
 
+
+def poincare_plot(fig, ax, data, vmax=1, trajectories=None, min_M = 0.0,
+                  col_offset=0.65):
+
+    grids = data['grids']
+    extent = [grids['states'][1][0],
+              grids['states'][1][-1],
+              grids['states'][0][0],
+              grids['states'][0][-1]]
+    # vmax = get_max_measure((data['S_M'] for data in data_list))
+    ax.imshow(data['S_M'], origin='lower', extent=extent, aspect='auto',
+                interpolation='bessel', vmin=0, vmax=vmax, cmap='viridis')
+    plt.title(str(np.round(data['p']['damping'], decimals=3)))
+
+    if trajectories is not None:
+        # X0 = [traj.y[:, 0] for traj in trajectories]
+        # XN = [traj.y[:, -1] for traj in trajectories]
+        # SN = [sys.xp2s_y_xdot(xn, data['p']) for xn in XN]
+
+        # s_grid_shape = list(map(np.size, grids['states']))
+        # # s_bin_shape = tuple(dim+1 for dim in s_grid_shape)
+        # SN_dig = [vibly.digitize_s(sn, grids['states']) for sn in SN]
+        # SNM = np.array([interp_measure(sbin, data['S_M'], grids)
+        #                 for sbin in SN_dig])
+        # ground_heights = [traj.y[-1, 0] for traj in trajectories]
+        # indices = np.arange(SNM.size)
+        # SNp = np.array(SN).T
+
+        # * get index of nominal trajectory, max step up/down
+        index0, max_up, max_down = get_perturbation_indices(trajectories)
+        # index0 = -1
+        # up_indices = list()
+        # down_indices = list()
+        # for idx, traj in enumerate(trajectories):
+
+
+        # * plot each step at next step
+        mycmap = plt.get_cmap("Blues")
+        for up_dx in range(index0):
+            traj = trajectories[up_dx]
+            xn = traj.y[:, -1]  # state at next step
+            sn = sys.xp2s_y_xdot(xn, data['p'])
+            sbin = vibly.digitize_s(sn, grids['states'])
+            s_m = interp_measure(sbin, data['S_M'], grids)
+            if s_m > min_M:
+                col = mycmap(col_offset-np.abs(xn[-1]))
+                ax.scatter(sn[1], sn[0],
+                           facecolors='none', edgecolors=col, s=20)
+
+        mycmap = plt.get_cmap("Reds")
+        for down_dx in range(index0+1, len(trajectories)):
+            traj = trajectories[down_dx]
+            xn = traj.y[:, -1]  # state at next step
+            sn = sys.xp2s_y_xdot(xn, data['p'])
+            sbin = vibly.digitize_s(sn, grids['states'])
+            s_m = interp_measure(sbin, data['S_M'], grids)
+            if s_m > min_M:
+                col = mycmap(col_offset-np.abs(xn[-1]))
+                ax.scatter(sn[1], sn[0],
+                           facecolors='none', edgecolors=col, s=20)
+
+        if index0 >= 0:
+            traj = trajectories[index0]
+            xn = traj.y[:, -1]  # state at next step
+            sn = sys.xp2s_y_xdot(xn, data['p'])
+            sbin = vibly.digitize_s(sn, grids['states'])
+            s_m = interp_measure(sbin, data['S_M'], grids)
+            ax.scatter(sn[1], sn[0],
+                       facecolors='none', edgecolors='black', s=20)
+        # mycmap = plt.get_cmap("Reds")
+        # for down_dx in range(index0+1, len(trajectories)):
+        #     traj = trajectories[down_dx]
+
+        #     x = traj.y[:, -1]
+        #     s = sys.xp2s_y_xdot(x, p)
+        #     sbin = vibly.digitize_s(s, grids['states'])
+        #     s_m = interp_measure(sbin, S_M, grids)
+
+        #     if s_m > 0.1:
+        #         col = mycmap(col_offset-np.abs(traj.y[-1, 0]))
+        #         ax.plot(traj.y[0], traj.y[1], color=col)
+
+        # if index0 >= 0:
+        #     traj = trajectories[index0]
+        #     ax.plot(traj.y[0], traj.y[1], color='k')
+        # if s_m > v_threshold:
+        #     col = mycmap(col_offset-np.abs(traj.y[-1, 0]))
+        #     ax.plot(traj.y[0], traj.y[1], color=col)
+
+        # for gdx, g in enumerate(ground_heights):
+        #     mycmap = plt.get_cmap("Blues")
+        #     ax.scatter(SNp[1, indices[SNM >= min_M]],
+        #                         SNp[0, indices[SNM >= min_M]],
+        #                         facecolors='none', edgecolors=[0.8, 0.3, 0.3],
+        #                         s=20)
--- scratchpad/daslip/damp_plots.py
+++ scratchpad/daslip/damp_plots.py
@@ -23,41 +23,101 @@ sns.set_context('poster')
 # * Flags for which plots to generate
 
 FLAG_TRAJS = False
-WATERFALL_PLOT = True
+WATERFALL_PLOT = False
+POINC_PLOT = True
 
 # * Load and partially parse data
 
-foldername = 'all_birds/'
-set_files = ['all_birds_0.01.pickle',
-             'all_birds_0.06.pickle',
-             'all_birds_0.1.pickle',
-             'all_birds_0.1.pickle',
-             'all_birds_0.15.pickle',
-             'all_birds_0.2.pickle']
+foldername = 'lowD/'
 
-traj_files = ['all_birds_0.01_trajs2.pickle',
-              'all_birds_0.06_trajs2.pickle',
-              'all_birds_0.1_trajs2.pickle',
-              'all_birds_0.1_trajs2.pickle',
-              'all_birds_0.15_trajs2.pickle',
-              'all_birds_0.2_trajs2.pickle']
+set_files = ['higher/higher_0.025.pickle',
+             'higher/higher_0.050.pickle',
+             'higher/higher_0.075.pickle',
+             'higher/higher_0.100.pickle',
+             'higher/higher_0.125.pickle',
+             'higher/higher_0.150.pickle',
+             'higher/higher_0.175.pickle',
+             'higher/higher_0.2.pickle',
+             'higher/higher_0.25.pickle',
+             'higher/higher_0.5.pickle',
+             'higher/higher_0.75.pickle',
+             'higher/higher_1.0.pickle',
+             'higher/higher_1.25.pickle',
+             'higher/higher_1.5.pickle',
+             'higher/higher_1.75.pickle',
+             'higher/higher_2.0.pickle',
+             'higher/higher_2.25.pickle',
+             'higher/higher_2.5.pickle',
+             'higher/higher_2.75.pickle',
+             'higher/higher_3.0.pickle',
+             'higher/higher_3.25.pickle',
+             'higher/higher_3.5.pickle']
 
-data = list()
-trajecs = list()
+traj_files = ['higher/higher_0.025_trajs2.pickle',
+              'higher/higher_0.050_trajs2.pickle',
+              'higher/higher_0.075_trajs2.pickle',
+              'higher/higher_0.100_trajs2.pickle',
+              'higher/higher_0.125_trajs2.pickle',
+              'higher/higher_0.150_trajs2.pickle',
+              'higher/higher_0.175_trajs2.pickle',
+              'higher/higher_0.2_trajs2.pickle',
+              'higher/higher_0.25_trajs2.pickle',
+              'higher/higher_0.5_trajs2.pickle',
+              'higher/higher_0.75_trajs2.pickle',
+              'higher/higher_1.0_trajs2.pickle',
+              'higher/higher_1.25_trajs2.pickle',
+              'higher/higher_1.5_trajs2.pickle',
+              'higher/higher_1.75_trajs2.pickle',
+              'higher/higher_2.0_trajs2.pickle',
+              'higher/higher_2.25_trajs2.pickle',
+              'higher/higher_2.5_trajs2.pickle',
+              'higher/higher_2.75_trajs2.pickle',
+              'higher/higher_3.0_trajs2.pickle',
+              'higher/higher_3.25_trajs2.pickle',
+              'higher/higher_3.5_trajs2.pickle']
+
+# set_files = ['test_0.005.pickle',
+# 'test_0.0065.pickle',
+# 'test_0.009.pickle',
+# 'test_0.0055.pickle',
+# 'test_0.008.pickle',
+# 'test_0.0095.pickle',
+# 'test_0.007.pickle',
+# 'test_0.0085.pickle',
+# 'test_0.006.pickle',
+# 'test_0.0075.pickle']
+
+# traj_files = ['test_0.005_trajs2.pickle',
+# 'test_0.0065_trajs2.pickle',
+# 'test_0.009_trajs2.pickle',
+# 'test_0.0055_trajs2.pickle',
+# 'test_0.008_trajs2.pickle',
+# 'test_0.0095_trajs2.pickle',
+# 'test_0.007_trajs2.pickle',
+# 'test_0.0085_trajs2.pickle',
+# 'test_0.006_trajs2.pickle',
+# 'test_0.0075_trajs2.pickle']
+
+data_list = list()
+trajec_list = list()
 for file_1, file_2 in zip(set_files, traj_files):
-    infile_1 = open(foldername+file_1, 'rb')
-    infile_2 = open(foldername+file_2, 'rb')
-    data.append(pickle.load(infile_1))
-    trajecs.append(pickle.load(infile_2)['trajectories'])
+    infile_1 = open(file_1, 'rb')
+    infile_2 = open(file_2, 'rb')
+    data_list.append(pickle.load(infile_1))
+    trajec_list.append(pickle.load(infile_2)['trajectories'])
     infile_1.close()
     infile_2.close()
 
-# Z[idx, :] = np.array(SNM)
+
+# * what to plot
+total_trials = len(data_list)
+# plot_these = [0, 5, 10, total_trials]
+plot_these = range(total_trials)
 
 # * compute measure after perturbation
 
-# XN = [traj.y[:, -1] for traj in data[idx]['trajectories']]
-# SN = [model.xp2s_y_xdot(xn, data[idx]['p']) for xn in XN]
+# XN = [traj.y[:, -1] for traj in data_list[idx]['trajectories']]
+# SN = [model.xp2s_y_xdot(xn, data_list[idx]['p']) for xn in XN]
 
 # * plot individual trajectories
 
@@ -66,7 +126,8 @@ if FLAG_TRAJS:
     index0 = -1
     max_up = 0
     max_down = 0
-    for idx, traj in enumerate(trajecs[0]):
+    tdx = 0
+    for idx, traj in enumerate(trajec_list[tdx]):
         if np.isclose(traj.y[-1, 0], 0):
             index0 = idx
 
@@ -78,28 +139,30 @@ if FLAG_TRAJS:
     #     print("WARNING: no nominal trajectory")
 
     num_up = index0-1
-    num_down = len(trajecs[0])-index0
+    num_down = len(trajec_list[tdx])-index0
 
     # height_cmap = sns.diverging_palette(10, 220, l=90, s=99, as_cmap=True, center='dark')
     # sns.color_palette("RdBu")
-    # plot_these = tuple(range(len(trajecs)))
+    # plot_these = tuple(range(len(trajec_list)))
     plot_these = (1, 2)
-    # trajectories = data[idx]['trajectories']
+    # trajectories = data_list[idx]['trajectories']
 
     for idx in plot_these:
         # fig = plt.figure(idx)
         fig, ax = plt.subplots()
-        daplot.ground_perturbations(ax, trajecs[idx], data[idx]['S_M'],
-                                    data[idx]['grids'], data[idx]['p'])
+        daplot.plot_ground_perturbations(ax, trajec_list[idx],
+                                         data_list[idx]['S_M'],
+                                         data_list[idx]['grids'],
+                                         data_list[idx]['p'])
     plt.show()
 
 # * plot waterfalls
 if WATERFALL_PLOT:
 
-    # prepare data
-    damping_values = np.array([d['p']['damping'] for d in data])
+    # prepare data_list
+    damping_values = np.array([d['p']['damping'] for d in data_list])
 
-    X0 = [traj.y[:, 0] for traj in trajecs[0]]
+    X0 = [traj.y[:, 0] for traj in trajec_list[0]]
     ground_heights = [x[-1] for x in X0]
 
     x = ground_heights
@@ -107,8 +170,8 @@ if WATERFALL_PLOT:
     X, Y = np.meshgrid(x, y)
     Z = np.zeros_like(X)
 
-    for idx in range(len(data)):
-        SNM = daplot.compute_measure_postep(data[idx], trajecs[idx])
+    for idx in range(len(data_list)):
+        SNM = daplot.compute_measure_postep(data_list[idx], trajec_list[idx])
         Z[idx, :] = np.array(SNM)
 
     # plot
@@ -132,3 +195,19 @@ if WATERFALL_PLOT:
     ax.yaxis.labelpad = 20
     ax.zaxis.labelpad = 12
     plt.show()
+
+if POINC_PLOT:
+
+    # find the maximum measure value, to normalize colormap
+    vmax = daplot.get_max_measure((data_list['S_M'] for data_list in data_list))
+
+    rem_string = len('.pickle')
+    for i, data in enumerate(data_list[i] for i in plot_these):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        daplot.poincare_plot(fig, ax, data, vmax=vmax,
+                             trajectories=trajec_list[i], min_M=0.0)
+        # plt.show()
+        new_filename = set_files[i][0:len(set_files[i])-rem_string]+'.pdf'
+        plt.savefig(new_filename, format='pdf')
+        plt.close()
