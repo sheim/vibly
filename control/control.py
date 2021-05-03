f337fa16f6766c1d376ab9ec13c7d4af3e3f68d0
Steve Heim <heim.steve@gmail.com>
2021-05-03 17:04:46 +0200

    improve value iteration: pre-compute Rewards once, and allow flexible handling of cell-to-grid for values

--- control/control.py
+++ control/control.py
@@ -34,7 +34,8 @@ def get_grid_indices(bin_idx, s_grid):
     return grid_coords
 
 def Q_value_iteration(Q_map, grids, reward_functions, gamma,
-                      stopping_threshold=1e-5, max_iter=1000):
+                      stopping_threshold=1e-5, max_iter=1000, output_R=False,
+                      neighbor_option=np.mean):
     """
     Standard value iteration.
     Inputs:
@@ -44,45 +45,54 @@ def Q_value_iteration(Q_map, grids, reward_functions, gamma,
     gamma: discount factor
     convergence_threshold: threshold on improvement to stop iterating
     max_iter: maximum number of iterations
+    output_R: toggle true to also return array of reward for each (s, a)
+    neighbor_option: how to interpolate grid-borders of a bin
     """
 
 
     Q_values = np.zeros_like(Q_map, dtype=float)
-    # S_Value = np.zeros([g.size for g in grids['states']])
+    # can be pre-computed one time
+    R_values = np.zeros_like(Q_map, dtype=float)
 
     s_grid = grids['states']
     a_grid = grids['actions']
     n_states = len(s_grid)
 
+    for qdx, next_s in np.ndenumerate(Q_map):
+        bin_idx = np.unravel_index(next_s, tuple(x+1 for x in map(np.size, s_grid)))
+        # pass transition through each reward function
+        reward = 0.0
+        s = [grid[qdx[i]] for i, grid in enumerate(s_grid)]
+        a = [grid[qdx[n_states + i]] for i, grid in enumerate(a_grid)]
+        for rfunc in reward_functions:
+            reward += rfunc(s, a)
+        R_values[qdx] = reward
+
+    # iterate over each q
     for iteration in range(max_iter):
-
-        # iterate over each q
         max_change = 0.0  # for stopping
-
         for qdx, next_s in np.ndenumerate(Q_map):
             bin_idx = np.unravel_index(next_s, tuple(x+1 for x in map(np.size, s_grid)))
-            # pass transition through each reward function
-            reward = 0.0
-            s = [grid[qdx[i]] for i, grid in enumerate(s_grid)]
-            a = [grid[qdx[n_states + i]] for i, grid in enumerate(a_grid)]
-            for rfunc in reward_functions:
-                reward += rfunc(s, a)
-            # update values
-            # if reward >= 0:
-                # print("hello")
             grid_indices = get_grid_indices(bin_idx, s_grid)
             # average bin value by neighboring q-values from grid
-            bin_value = 0.0
-            for g in grid_indices:
-                bin_value += Q_values[g].max()/len(grid_indices)
+            # bin_value = 0.0
+            # for g in grid_indices:
+            #     bin_value += Q_values[g].max()/len(grid_indices)
+            bin_value = neighbor_option([Q_values[g].max()
+                                        for g in grid_indices])
+
 
             # keep track fo changes, for stopping condit    ion
-            diff = np.abs(Q_values[qdx] - reward - gamma*bin_value)
+            diff = np.abs(Q_values[qdx] - R_values[qdx] - gamma*bin_value)
             if (diff > max_change):
                 max_change = diff
-            Q_values[qdx] = reward + gamma*bin_value
+            Q_values[qdx] = R_values[qdx] + gamma*bin_value
         if max_change < stopping_threshold:
             print("Stopped early after ", iteration, " iterations.")
             break
     print("max change in value: ", max_change)
-    return Q_values
+
+    if output_R:
+        return Q_values, R_values
+    else:
+        return Q_values
