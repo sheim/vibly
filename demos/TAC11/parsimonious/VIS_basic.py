3101f8a0efdd7af350efa8fc0c7c7e8b6be978df
Steve Heim <heim.steve@gmail.com>
2021-06-04 21:26:33 +0200

    add all code needed to reproduce TAC submission

---
+++ demos/TAC11/parsimonious/VIS_basic.py
@@ -0,0 +1,154 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import viability as vibly  # algorithms for brute-force viability
+from ttictoc import tictoc
+import control
+import plotting.value_plotters as vplot
+
+import pickle
+import os
+
+# * here we choose the parameters to use
+# * we also put in a place-holder
+#  action (thrust)
+
+pathname = '../../../data/dynamics/'
+basename = 'closed_satellite11'
+# basename = 'test_satellite'
+filename = pathname+basename
+infile = open(filename+'.pickle', 'rb')
+data = pickle.load(infile)
+infile.close()
+
+# * unpack
+grids = data["grids"]
+Q_map = data["Q_map"]
+Q_F = data["Q_F"]
+Q_V = data["Q_V"]
+Q_M = data["Q_M"]
+S_M = data["S_M"]
+p = data["p"]
+Q_on_grid = np.ones(Q_map.shape, dtype=bool)
+
+XV = S_M>0.0
+
+def parsimonious_reward(s, a):
+    if np.allclose(s, [10., 0.]):
+        return 1.
+    else:
+        return 0.
+
+s0_weight = 1/(grids['states'][0][-1] - grids['states'][0][0])
+s1_weight = 1/(grids['states'][1][-1] - grids['states'][1][0])
+def target(s, a):
+    d = np.hypot(s0_weight*(s[0]-10.), s1_weight*s[1])
+    if d <= 0.05:
+        return 1.
+    else:
+        return 0.
+
+actuator_penalty = 1.0
+def actuator_cost(s, a):
+    return -actuator_penalty*a[0]**2
+
+def proxy_penalty_speed(s, a):
+    if s[1] >= -2. and s[1] <= 2:
+        reward = 0.
+    else:
+        reward = -1.
+    return reward
+
+failure_penalty = 1.0
+def penalty(s, a):
+    if s[0] >= p['radio_range']:
+        return -failure_penalty
+    elif s[0] <= p['radius']:
+        return -failure_penalty
+    else:
+        return 0.
+
+reward_functions = (parsimonious_reward, penalty)
+savename = './negproxy'
+# savename = './closed21/orti/TEST'
+# reward_schemes = ((target,),
+#                   (penalty,))
+
+###########################################################################
+# * save data as pickle
+###########################################################################
+
+mymap = vplot.get_vmap()
+# mynorm = colors.CenteredNorm()
+
+failure_penalties = [0., 1.]
+# failure_penalties = np.arange(2., 10., 1.)
+# failure_penalties = np.arange(131., 140., 1.)
+# for rdx, reward_functions in enumerate(reward_schemes):
+Q_value = None
+for failure_penalty in failure_penalties:
+    tictoc.tic()
+    if Q_value is not None:
+        Q_value *= failure_penalty
+    Q_value, R_value = control.Q_value_iteration(Q_map, grids,
+                            reward_functions, 0.6, Q_on_grid=Q_on_grid,
+                            stopping_threshold=1e-6, max_iter=1000,
+                            output_R=True, Q_values=Q_value)
+    X_value = vibly.project_Q2S(Q_value, grids, proj_opt=np.max)
+
+    time_elapsed = tictoc.toc()
+    print("time elapsed (minutes): " + str(time_elapsed/60.0))
+
+    data2save = {"Q_value": Q_value, "R_value": R_value,
+                "grids": grids, "Q_map": Q_map, "Q_F": Q_F, "Q_V": Q_V,
+                "Q_M": Q_M, "S_M": S_M, "p": p}
+    outfile = open(savename+str(int(failure_penalty))+
+                   '.pickle', 'wb')
+    pickle.dump(data2save, outfile)
+    outfile.close()
+
+
+    # * plot
+    print("============")
+    print("PENALTY: ", failure_penalty)
+    print("Min viable value: ", X_value[XV].min())
+    print("Max unviable value: ", X_value[~XV].max())
+    XV0 = X_value>=X_value[XV].min()
+    print("XV0 min value: ", X_value[XV0].min())
+    if np.any(~XV0):
+        print("Outside XV0 max value: ", X_value[~XV0].max())
+    print("Size of XV0: ", XV0.astype(int).sum())
+
+    # viability_threshold = X_value[XV].min()
+
+    # mynorm = colors.CenteredNorm()
+    # mymap = plt.get_cmap('bwr_r')
+    # shrunk_cmap = vplot.shiftedColorMap(mymap, start=0.2, midpoint=0.5, stop=0.8, name='shrunk')
+
+    extent = [grids['states'][1][0],
+            grids['states'][1][-1],
+            grids['states'][0][0],
+            grids['states'][0][-1]]
+
+    fig, axs = plt.subplots(2, 1)
+
+    # * Reward function
+    RX_value = vibly.project_Q2S(R_value, grids, proj_opt=np.mean)
+
+    pc0 = vplot.reward_function(axs[0], RX_value, grids, XV=XV,
+                                mymap="PRGn")
+    fig.colorbar(pc0, ax=axs[0])
+
+    # * Value Function
+
+    pc1 = vplot.value_function(axs[1], X_value, grids, XV=XV,
+                               mymap=mymap)
+    fig.colorbar(pc1, ax=axs[1])
+    plt.savefig(savename+str(failure_penalty)+'.pdf',
+                format='pdf')
+
+    print("******************")
+    print("******************")
+    # plt.imshow(np.transpose(S_M), origin='lower')  # visualize the S-safety measure
+    # plt.show()
+    # plt.imshow(Q_V) # visualize the viable set
+    # plt.show()
---
+++ demos/TAC11/pos_proxy/plot_split_posproxy.py
@@ -0,0 +1,109 @@
+import numpy as np
+
+import matplotlib as cm
+cm.use("TkAgg")
+cm.rcParams['pdf.fonttype'] = 42
+cm.rcParams['ps.fonttype'] = 42
+import matplotlib.colors as colors
+import matplotlib.cbook as cbook
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import viability as vibly  # algorithms for brute-force viability
+# from mpl_toolkits.mplot3d import Axes3D
+# from matplotlib import rcParams
+# from scipy.signal import savgol_filter
+import pickle
+import os
+import matplotlib.collections as collections
+
+import plotting.value_plotters as vplot
+colwide = 3.5
+pagewide = 7.16
+# import seaborn as sns
+# sns.set_theme(style="darkgrid")
+
+# ! p* at 111
+# * Load data
+alpha = 0.
+
+filenames = [#"posproxy0.pickle",
+             "posproxy1.pickle",
+             "posproxy111.pickle"]
+
+# filenames = ["posproxy0.pickle",
+#             "shaping/L2arti_penalty_10.pickle",
+#             "shaping/L2arti_penalty_50.pickle"]
+
+data_list = list()
+
+try:
+    for filename in filenames:
+        infile = open(filename, 'rb')
+        data_list.append(pickle.load(infile))
+        infile.close()
+except FileNotFoundError:
+    print("ERROR: data not found.")
+
+# * unpack invariants
+# Q_value = data["Q_value"]
+R_value = data_list[0]["R_value"]
+grids = data_list[0]["grids"]
+# Q_map = data["Q_map"]
+S_M = data_list[0]["S_M"]
+
+# * Ground Truth XV
+XV = S_M > 0.0
+
+mynorm = colors.TwoSlopeNorm(vmin=-100, vcenter=0.0, vmax=2.5)
+# mynorm = colors.CenteredNorm()
+mymap = vplot.get_vmap(0)
+# mymap = np.vstack((plt.cm.Blues_r(np.linspace(0.2, 0.7, 127)),
+#                    plt.cm.Oranges(np.linspace(0.3, 0.8, 128))))
+# mymap = colors.LinearSegmentedColormap.from_list('my_colormap', mymap)
+extent = [grids['states'][1][0],
+        grids['states'][1][-1],
+        grids['states'][0][0],
+        grids['states'][0][-1]]
+
+# these_values = [0, 3, 5, -3, -2, -1]
+# these_values = range(len(data_list))
+# num_plots = len(data_list)+1
+# fig, axs = plt.subplots(num_plots, 1, figsize = (colwide, 16/10*colwide))
+figR, axR = plt.subplots(figsize=(colwide, 3/6*colwide), constrained_layout=True)
+# * plot reward function
+R_value = data_list[0]["R_value"]
+RX_value = vibly.project_Q2S(R_value, grids, proj_opt=np.mean)
+
+
+# colors.LinearSegmentedColormap.from_list('second_map', mymap)
+# newcolors = [(0., 0., 0., 1), (1., 1., 1., 1.), mymap(mynorm(1.0))]
+rmap = colors.ListedColormap(["red", "white", "darkgrey"])
+# rmap = colors.ListedColormap(newcolors)
+pcR = vplot.reward_function(axR, RX_value, grids, XV=XV, mymap=rmap)
+axR.set_xlabel("$x_2$")
+axR.set_ylabel("$x_1$")
+figR.colorbar(pcR, ax=axR, orientation='horizontal', extend='both',
+                spacing='proportional', shrink=1)
+plt.savefig('posproxy_R.pdf', format='pdf')
+plt.close()
+# * plot value functions
+lvls = [-100., -50., alpha, 1., 2.]
+for pdx, data in enumerate(data_list):
+    fig, ax = plt.subplots(figsize=(colwide, 3/6*colwide), constrained_layout=True)
+    X_value = vibly.project_Q2S(data["Q_value"], grids, proj_opt=np.max)
+    pc1 = vplot.value_function(ax,
+                                X_value,
+                                grids,
+                                XV=XV, cont_lvls=lvls,
+                                mymap=mymap,
+                                mynorm=mynorm)
+    print("max: ", X_value.max())
+    print("min viable: ", X_value[XV].min())
+    print("max unviable: ", X_value[~XV].max())
+    fig.colorbar(pc1, ax=ax, orientation='horizontal', extend='both',
+                spacing='proportional', ticks=lvls, shrink=0.8)
+    ax.set_xlabel("$x_2$")
+    ax.set_ylabel("$x_1$")
+    plt.savefig('Tposproxy_Vp_'+str(pdx)+'.pdf', format='pdf')
+# plt.show()
+plt.close()
